fb0dcaac8c2337e79c102a1e7fcaa14a8e05a05b
vedala srinivas <vedalasrinivas@vedalas-MacBook-Pro.local>
2017-09-20 12:39:23 +0530

    Second commit

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
-from flask import Flask
-
+from flask import Flask,request
+from flask_mysqldb import MySQL 
+mysql = mySQL()
 app = Flask(__name__)
+app.config['MYSQL_HOST'] = 'databases.000webhost.com'
+app.config['MYSQL_USER'] = 'id2597668_saivicky'
+app.config['MYSQL_PASSWORD'] = 'chelamela'
+app.config['MYSQL_DB'] = 'id2597668_vicky'
+mysql.init_app(app)
 
 @app.route('/')
 def index():
-	return '<h1>Deployed</h1>'
+	return Autenticate()
+@app.route('/Authenticate')
+def Authenticate():
+	username = request.args.get('Username')
+	password = request.args.get('Password')
+	cursor = mysql.connect().cursor()
+	cursor.execute("SELECT* FROM l_available")
+	data = cursor.fetchone()
+	if data is None :
+		return "Wrong username"
+	else
+		return data
+
