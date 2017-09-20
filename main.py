252bf93eab6a576b22c64ff6ed0c623b913b23c9
vedala srinivas <vedalasrinivas@vedalas-MacBook-Pro.local>
2017-09-20 19:19:56 +0530

    Temp commit

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import pymysql
-pymysql.install_as_MySQLdb()
-from flask import Flask,request 
-from flask_mysqldb import MySQL
+#pymysql.install_as_MySQLdb()
+from flask import Flask,render_template
+import MySQLdb
 
 app = Flask(__name__)
-mysql = MySQL(app)
-app.config['MYSQL_HOST'] = 'databases.000webhost.com'
-app.config['MYSQL_USER'] = 'id2597668_saivicky'
-app.config['MYSQL_PASSWORD'] = 'chelamela'
-app.config['MYSQL_DB'] = 'id2597668_vicky'
-mysql.init_app(app)
+#mysql = MySQL(app)
+#app.config['MYSQL_HOST'] = 'databases.000webhost.com'
+#app.config['MYSQL_USER'] = 'id2597668_saivicky'
+#app.config['MYSQL_PASSWORD'] = 'chelamela'
+#app.config['MYSQL_DB'] = 'id2597668_vicky'
+#mysql.init_app(app)
+db = MySQLdb.connect(host='databases.000webhost.com',user='id2597668_saivicky',passwd='chelamela',db='id2597668_vicky')
 
 @app.route('/')
 def index():
 	return '<h1> Deployed </h1>'
 
-@app.route('/Authenticate')
+"""@app.route('/Authenticate')
 def Authenticate():
-	username = request.args.get('Username')
-	password = request.args.get('Password')
-	cursor = mysql.connection.cursor()
+	cursor = db.cursor()
 	cursor.execute("SELECT* FROM l_available")
-	data = cursor.fetchone()
+	data = cursor.fetchall()
 	if data is None :
-		return "Wrong username"
+		return '<h1>Wrong username</h1>'
 	else :
-		return data
+		return render_template('index.html')
+		
 
+if __name__ == "__main__":
+    app.run()
+"""
