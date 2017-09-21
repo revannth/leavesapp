21d74b05b7dac162f37399eeaa27dc4bd452c66b
Revannth <Revannth@vedalas-MacBook-Pro.local>
2017-09-21 10:48:00 +0530

    Sixth Commit

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
-#import pymysql
-#pymysql.install_as_MySQLdb()
+import pymysql
+pymysql.install_as_MySQLdb()
 from flask import Flask,render_template
-#import MySQLdb
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
-#mysql = MySQL(app)
-#app.config['MYSQL_HOST'] = 'databases.000webhost.com'
-#app.config['MYSQL_USER'] = 'id2597668_saivicky'
-#app.config['MYSQL_PASSWORD'] = 'chelamela'
+mysql = MySQL(app)
+app.config['MYSQL_HOST'] = 'us-cdbr-iron-east-05.cleardb.net'
+app.config['MYSQL_USER'] = 'ba6ad320cb67d2'
+app.config['MYSQL_PASSWORD'] = 'd44c8e9f'
 #app.config['MYSQL_DB'] = 'id2597668_vicky'
-#mysql.init_app(app)
+mysql.init_app(app)
 #db = MySQLdb.connect(host='databases.000webhost.com',user='id2597668_saivicky',passwd='chelamela',db='id2597668_vicky')
 
 @app.route('/')
 def index():
 	return '<h1> Deployed </h1>'
 
-"""@app.route('/Authenticate')
+@app.route('/Authenticate')
 def Authenticate():
 	cursor = db.cursor()
 	cursor.execute("SELECT* FROM l_available")
@@ -25,7 +25,7 @@ def Authenticate():
 		return '<h1>Wrong username</h1>'
 	else :
 		return render_template('index.html')
-"""	
+	
 
 #if __name__ == "__main__":
 #   app.run()
