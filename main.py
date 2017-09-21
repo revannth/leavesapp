680361545e581169a0a9d6168e198659f99c8716
Revannth <Revannth@vedalas-MacBook-Pro.local>
2017-09-21 11:09:12 +0530

    Seventh

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ mysql = MySQL(app)
 app.config['MYSQL_HOST'] = 'us-cdbr-iron-east-05.cleardb.net'
 app.config['MYSQL_USER'] = 'ba6ad320cb67d2'
 app.config['MYSQL_PASSWORD'] = 'd44c8e9f'
+app.config['MYSQL_DB'] = 'heroku_fa71028ec71c3a9'
 #app.config['MYSQL_DB'] = 'id2597668_vicky'
 mysql.init_app(app)
 #db = MySQLdb.connect(host='databases.000webhost.com',user='id2597668_saivicky',passwd='chelamela',db='id2597668_vicky')
@@ -18,15 +19,16 @@ def index():
 
 @app.route('/Authenticate')
 def Authenticate():
-	cursor = db.cursor()
+
+	cursor = mysql.connect.cursor()
 	cursor.execute("SELECT* FROM l_available")
 	data = cursor.fetchall()
 	if data is None :
 		return '<h1>Wrong username</h1>'
 	else :
-		return render_template('index.html')
+		return render_template('index.html',data=data)
 	
 
-#if __name__ == "__main__":
-#   app.run()
-
+'''if __name__ == "__main__":
+	app.run()
+'''
